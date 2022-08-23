c5d79d00c87909cc6789f30f4a1e963a2a936e8b
DxsSucuk <dxssucuk@hotmail.com>
2022-08-23 22:38:17 +0200

    Unfinished Andrew Tate Easter Egg

---
+++ Assets/Scripts/Entity/AndrewTateInteractable.cs
@@ -0,0 +1,19 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+namespace Entity
+{
+    public class AndrewTateInteractable : Interactable
+    {
+        public GameObject prefab;
+        
+        public override void InteractWithInteractable()
+        {
+            if (usable)
+            {
+                PhotonNetwork.InstantiateRoomObject(prefab.name, transform.position, Quaternion.identity);
+                usable = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Entity/Interactable.cs
@@ -0,0 +1,16 @@
+using Photon.Pun;
+using UnityEngine;
+
+public class Interactable : MonoBehaviourPunCallbacks
+{
+    public float interactableArea = 5f;
+    public bool usable = true;
+
+    public virtual void InteractWithInteractable() {}
+    
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position,interactableArea);
+    }
+}
